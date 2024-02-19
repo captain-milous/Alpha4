3285a3bd16eee82173f539f29bb145bd9e7da31c
captain-milous <milda.tesar3@gmail.com>
2024-02-19 14:14:15 +0100

    dsafdsdf

--- P2P Chat/src/Program.cs
+++ P2P Chat/src/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Text;
 using Newtonsoft.Json;
+using P2P_Chat.src.client;
 
 namespace P2P_Chat.src
 {
@@ -10,80 +11,14 @@ namespace P2P_Chat.src
         static void Main(string[] args)
         {
             // Spustíme vlákno pro periodické odesílání JSON dotazu
-            Thread senderThread = new Thread(SenderThread);
+            Thread senderThread = new Thread(ClientUDP.SenderThread());
             senderThread.Start();
 
             // Spustíme vlákno pro příjem JSON dotazů
-            Thread receiverThread = new Thread(ReceiverThread);
+            Thread receiverThread = new Thread(ClientUDP.ReceiverThread());
             receiverThread.Start();
         }
 
-        static void SenderThread()
-        {
-            while (true)
-            {
-                try
-                {
-                    // Vytvoříme JSON dotaz
-                    var query = new { message = "Hello from peer" };
-                    string jsonQuery = JsonConvert.SerializeObject(query);
-
-                    Console.WriteLine("Q: " + jsonQuery);
-
-                    // Odešleme JSON dotaz pomocí UDP broadcastu
-                    using (var client = new UdpClient())
-                    {
-                        client.EnableBroadcast = true;
-                        IPEndPoint endPoint = new IPEndPoint(IPAddress.Broadcast, 9876);
-                        byte[] bytes = Encoding.ASCII.GetBytes(jsonQuery);
-                        client.Send(bytes, bytes.Length, endPoint);
-                    }
-                } 
-                catch (Exception e)
-                {
-                    Console.WriteLine("Sender error: " + e.ToString());
-                }
-                
-
-                // Počkáme 5 sekund
-                Thread.Sleep(5000);
-            }
-        }
-
-        static void ReceiverThread()
-        {
-            // Vytvoříme UDP listener na portu 9876
-            var listener = new UdpClient(9876);
-
-            while (true)
-            {
-                try
-                {
-                    // Přijmeme zprávu
-                    IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
-                    byte[] receivedBytes = listener.Receive(ref endPoint);
-                    string receivedJson = Encoding.ASCII.GetString(receivedBytes);
-
-                    // Deserializujeme přijatý JSON dotaz
-                    dynamic receivedQuery = JsonConvert.DeserializeObject(receivedJson);
-                    string message = receivedQuery.message;
-
-                    // Vytvoříme JSON odpověď
-                    var response = new { responseMessage = "Hi, I received your message: " + message };
-                    string jsonResponse = JsonConvert.SerializeObject(response);
-
-                    Console.WriteLine("A: " + jsonResponse);
-
-                    // Odešleme JSON odpověď zpět na stejnou adresu, ze které jsme obdrželi dotaz
-                    byte[] responseBytes = Encoding.ASCII.GetBytes(jsonResponse);
-                    listener.Send(responseBytes, responseBytes.Length, endPoint);
-                } 
-                catch (Exception e)
-                {
-                    Console.WriteLine("Receiver error: " + e.ToString());
-                }
-                
-            }
-        }
+        
     }
 }
---
+++ P2P Chat/src/client/ClientUDP.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2P_Chat.src.client
+{
+    public static class ClientUDP
+    {
+        public static void SenderThread()
+        {
+            while (true)
+            {
+                Console.WriteLine();
+                try
+                {
+                    // Vytvoříme JSON dotaz
+                    var query = new { message = "Hello from peer" };
+                    string jsonQuery = JsonConvert.SerializeObject(query);
+
+                    Console.WriteLine("Q: " + jsonQuery);
+
+                    // Odešleme JSON dotaz pomocí UDP broadcastu
+                    using (var client = new UdpClient())
+                    {
+                        client.EnableBroadcast = true;
+                        IPEndPoint endPoint = new IPEndPoint(IPAddress.Broadcast, 9876);
+                        byte[] bytes = Encoding.ASCII.GetBytes(jsonQuery);
+                        client.Send(bytes, bytes.Length, endPoint);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Sender error: " + e.Message);
+                }
+
+
+                // Počkáme 5 sekund
+                Thread.Sleep(5000);
+            }
+        }
+
+        public static void ReceiverThread()
+        {
+            // Vytvoříme UDP listener na portu 9876
+            var listener = new UdpClient(9876);
+
+            while (true)
+            {
+                try
+                {
+                    // Přijmeme zprávu
+                    IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+                    byte[] receivedBytes = listener.Receive(ref endPoint);
+                    string receivedJson = Encoding.ASCII.GetString(receivedBytes);
+
+                    // Deserializujeme přijatý JSON dotaz
+                    dynamic receivedQuery = JsonConvert.DeserializeObject(receivedJson);
+                    string message = receivedQuery.message;
+
+                    // Vytvoříme JSON odpověď
+                    var response = new { responseMessage = "Hi, I received your message: " + message };
+                    string jsonResponse = JsonConvert.SerializeObject(response);
+
+                    Console.WriteLine("A: " + jsonResponse);
+
+                    // Odešleme JSON odpověď zpět na stejnou adresu, ze které jsme obdrželi dotaz
+                    byte[] responseBytes = Encoding.ASCII.GetBytes(jsonResponse);
+                    listener.Send(responseBytes, responseBytes.Length, endPoint);
+                }
+                catch (Exception e)
+                {
+                    //Console.WriteLine("Receiver error: " + e.Message);
+                }
+
+            }
+        }
+    }
+}
